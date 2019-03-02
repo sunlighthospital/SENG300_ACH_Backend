39d47e82949c62c7a94efcb508606f5dd09c09e8
SomeoneInParticular <kalumost@gmail.com>
2019-03-02 14:42:15 -0700

    Changed error handling code to not trick server into ignoring functions for retrieval

--- src/com/ach_manager/db/TestScript.java
+++ src/com/ach_manager/db/TestScript.java
@@ -1,7 +1,6 @@
 package com.ach_manager.db;
 
 import java.sql.SQLException;
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
@@ -9,11 +8,25 @@ import javax.ws.rs.core.MediaType;
 public class TestScript {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String test() throws SQLException {
+	public String test(){
 		String test_string = "";
-		test_string += AppointmentManager.getDocAppointmentsByDocID(1);
+		try {
+			test_string += AppointmentManager.getDocAppointmentsByDocID(1);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		test_string += "----------------------------------------------\n";
-		test_string += DoctorManager.getAllDoctorsInDepByID(2);
+		try {
+			test_string += DoctorManager.getAllDoctorsInDepByID(2);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return test_string;
+	}
+	
+	public static void main(String args[]) {
+		System.out.println("Fuck this");
 	}
 }
