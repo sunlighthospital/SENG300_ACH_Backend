a8a66ec48b6f201f3949660fd0e2c3c2cb1ff226
SomeoneInParticular <kalumost@gmail.com>
2019-03-12 03:57:10 -0600

    Added credential check with database. API still to be implimented.

---
+++ src/com/ach_manager/db/CredentialManager.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ach_manager.db;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONObject;
+
+/**
+ *
+ * @author kalum
+ */
+public class CredentialManager {
+    // Checks if a user's credentials are in the database
+    // If they are, returns a JSON file containing their name, phone number, and department (if any)
+    // 
+    public JSONObject checkCredentials(String user, String pass) {
+        // Initialize Connection
+        Connection con = ConnectionManager.getConnection();
+        // Holds results from query
+        Statement stmt = null;
+        // JSON representation of the schedule
+        JSONObject results = new JSONObject();
+        // Holds results from query
+        ResultSet rs = null;
+        // Query assembly (mySQL format)
+        String query = "SELECT c.name, p.name AS dep_name "
+                + "FROM credential c "
+                + "LEFT JOIN doctor d ON c.id = d.cred_id "
+                + "LEFT JOIN department p ON d.dep_id = p.id "
+                + "WHERE c.username = '" + user + "' "
+                + "AND c.password = '" + pass + "';";
+        try {
+            // Attempt the query
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
+            // Add the results to the json object parametes
+            if (rs.next()) {
+                results.put("name", rs.getString("name"));
+                results.put("department", rs.getString("dep_name"));
+            } else {
+                results.put("ERROR", "CREDENTIALS NOT FOUND (RESPONSE.NOT_FOUND)");exit
+            }
+            // Close the connections to avoid memory leaks
+            stmt.close();
+            con.close();
+        } catch (SQLException e) {
+            results = null;
+            System.out.println(e);
+        }
+        return results;
+    }
+}
--- src/com/ach_manager/db/TestScript.java
+++ src/com/ach_manager/db/TestScript.java
@@ -11,6 +11,7 @@ public class TestScript {
     // Variable instantiation
     private final AppointmentManager app_man = new AppointmentManager();
     private final DoctorManager doc_man = new DoctorManager();
+    private final CredentialManager cred_man = new CredentialManager();
     
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
@@ -20,6 +21,8 @@ public class TestScript {
             test_json.put("Val1", app_man.getDocAppointmentsByDocID(4));
             test_json.put("Val2", doc_man.getAllDoctorsInDepByID(2));
             test_json.put("Val3", doc_man.getAllDoctors());
+            test_json.put("Val4", cred_man.checkCredentials("AndyPants", "pass1"));
+            test_json.put("Val5", cred_man.checkCredentials("FakeyMcFakerson", "lolno"));
         } catch (SQLException e) {
             test_json = null; 
             e.printStackTrace();
