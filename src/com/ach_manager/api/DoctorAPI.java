13331fc9d5baed67ed8d52a94f0b9c4298b5a97e
SomeoneInParticular <kalumost@gmail.com>
2019-03-16 01:39:58 -0600

    Fixed bug; forgot to return string instead of JSON

--- src/com/ach_manager/api/DoctorAPI.java
+++ src/com/ach_manager/api/DoctorAPI.java
@@ -9,9 +9,7 @@ import com.ach_manager.db.DoctorManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -27,14 +25,14 @@ public class DoctorAPI {
     @Path("/all")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public JSONObject getAll() {
+    public String getAll() {
         try {
             JSONObject jo = doc_man.getAllDoctors();
-            return jo;
+            return jo.toString();
         }
         catch (Exception e){
             e.printStackTrace();
         }
-        return null;
+        return new JSONObject().toString();
     }
 }
