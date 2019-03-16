55721159c04b7d64f9d5aad4ad9122408759c4dd
Syed Jafri <syed.jafri2@ucalgary.ca>
2019-03-16 03:06:14 -0600

    add CORS

--- db_test_data/setup.sql
+++
@@ -1,7 +0,0 @@
---
--- Interfacing User addition
---
-DROP USER IF EXISTS 'client'@'localhost';
-CREATE USER 'client'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON SENG300_Hospital.* TO 'client'@'localhost';
-FLUSH PRIVILEGES;
